60951ff1b5c1dff19ee3a56f54954f2e41435c03
Varga Dániel <vargadaniel001@gmail.com>
2023-12-03 17:14:19 +0100

    search by ingredient

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from "express";
 import axios from "axios";
 import bodyParser from "body-parser";
 import morgan from "morgan";
+import _ from "lodash";
 
 const app = express();
 const port = 3000;
@@ -51,12 +52,57 @@ app.post("/cocktail", async (req, res) => {
       count += 1;
     }
     res.render("sCocktail.ejs", {
-        content: randDrink,
-        ingredients: ingredients,
-        ingredientsList: ingredientsList,
-      });
+      content: randDrink,
+      ingredients: ingredients,
+      ingredientsList: ingredientsList,
+    });
   } catch (error) {
     res.status(404).send(error.message);
+  }
+});
+
+app.post("/ingredient", async (req, res) => {
+  const ingredientName = req.body.iInput;
+  try {
+    const result = await axios.get(
+      `https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${ingredientName}`
+    );
+    const randDrink = result.data.drinks;
+    res.render("iCocktail.ejs", {
+      content: _.sampleSize(randDrink, 12),
+      ingredient: ingredientName,
+    });
+  } catch (error) {
+    res.status(404).send(error.message);
+  }
+});
+
+app.get("/drink/:id", async (req, res) => {
+  const drinkId = req.params["id"];
+  try {
+    const result = await axios.get(
+      `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${drinkId}`
+    );
+    const randDrink = result.data.drinks[0];
+    let ingredients = [];
+    let ingredientsList = [];
+    let count = 1;
+    while (randDrink["strIngredient" + count]) {
+      ingredients.push(
+        randDrink["strMeasure" + count] +
+          " " +
+          randDrink["strIngredient" + count]
+      );
+      ingredientsList.push(randDrink["strIngredient" + count]);
+      count += 1;
+    }
+    res.render("Cocktail.ejs", {
+      content: randDrink,
+      ingredients: ingredients,
+      ingredientsList: ingredientsList,
+    });
+  } catch (error) {
+    console.log(error.message);
   }
 });
 
